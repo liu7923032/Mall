8b53e4b86f5ab8aea2ca2728d2aac103f9974c93
unknown <1137411088@qq.com>
2017-11-03 22:17:42 +0800

    change index to use layui templete

--- src/Mall.EntityFrameworkCore/Migrations/20171103122333_Initlize_new.Designer.cs
+++ src/Mall.EntityFrameworkCore/Migrations/20171103122333_Initlize_new.Designer.cs
@@ -12,9 +12,10 @@ using System;
 namespace Mall.Migrations
 {
     [DbContext(typeof(MallDbContext))]
-    partial class MallDbContextModelSnapshot : ModelSnapshot
+    [Migration("20171103122333_Initlize_new")]
+    partial class Initlize_new
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ src/Mall.EntityFrameworkCore/Migrations/20171103122333_Initlize_new.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace Mall.Migrations
+{
+    public partial class Initlize_new : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Integral",
+                table: "Mall_Integral",
+                type: "decimal(18, 2)",
+                nullable: false,
+                oldClrType: typeof(double));
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Integral",
+                table: "Mall_Account",
+                type: "decimal(18, 2)",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldNullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<double>(
+                name: "Integral",
+                table: "Mall_Integral",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18, 2)");
+
+            migrationBuilder.AlterColumn<long>(
+                name: "Integral",
+                table: "Mall_Account",
+                nullable: true,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(18, 2)",
+                oldNullable: true);
+        }
+    }
+}
